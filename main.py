a7f33a5140b64284ccd4d44938e10d50e835d68e
OFDLLai <axd5201211@gmail.com>
2017-04-22 23:08:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,18 @@
 
 import smtplib
 from email.mime.text import MIMEText
-from email.header import Header
-
 
 Send_host='smtp.gmail.com'  #Set up your Smtp server
 Send_user='xxxxxx@gmail.com'    #Input send User
 Send_pass='******'   #Input send User password 
 
 sender = 'lai'  #Input send User
-receivers = 'zzzzzz@gmail.com'  # Receive User
+recipient = 'zzzzzz@gmail.com'  # Receive User
 
-message = MIMEText('Python Email Test...', 'plain', 'utf-8')
-message['From'] = Send_user
-message['To'] =  'Python Test'
-
-subject = 'Python SMTP TEST'
-message['Subject'] = Header(subject, 'utf-8')
+msg = MIMEText('Python Email Test', 'plain', 'utf-8')
+msg['From'] = Send_user
+msg['To'] =  'Python Test'
+msg['Subject'] = 'Python SMTP TEST'
 
 
 try:
@@ -27,7 +23,7 @@ try:
     start_send.ehlo()
     start_send.starttls()
     start_send.login(Send_user,Send_pass)  
-    start_send.sendmail(sender, receivers, message.as_string())
+    start_send.sendmail(sender, recipient, msg.as_string())
     start_send.quit()
     print ("Send Successful")
 except smtplib.SMTPException:
